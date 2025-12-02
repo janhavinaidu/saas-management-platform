6cf39abd3d7f8df2b0d37b7ebd4b137ead702142
janhavinaidu <janhavinaidu123@gmail.com>
2025-12-02 21:00:36 +0530

    Move requirements.txt to project root

--- backend/saas_project/settings.py
+++ backend/saas_project/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os # Import os for environment variables
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -86,12 +86,7 @@ WSGI_APPLICATION = 'saas_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = {'default': dj_database_url.config(conn_max_age=600)}
 
 
 # Password validation
@@ -119,6 +114,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# --- ADD THIS LINE FOR PRODUCTION DEPLOYMENT ---
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
