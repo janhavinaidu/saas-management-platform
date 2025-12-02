9175501501f00db0870fe48a331f71ad9ea28c72
janhavinaidu <janhavinaidu123@gmail.com>
2025-12-02 22:34:21 +0530

    Fix DATABASES default config fallback

--- backend/saas_project/settings.py
+++ backend/saas_project/settings.py
@@ -86,7 +86,14 @@ WSGI_APPLICATION = 'saas_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {'default': dj_database_url.config(conn_max_age=600)}
+DATABASES = {
+    'default': dj_database_url.config(
+        default="sqlite:///" + str(BASE_DIR / "db.sqlite3"),
+        conn_max_age=600,
+        ssl_require=False
+    )
+}
+
 
 
 # Password validation
